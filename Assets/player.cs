11ffa4026c9059ea91b886da2a85e56130e7e9a5
rosaParvum <nihilisticrealist@gmail.com>
2022-10-11 14:33:20 +0100

    animation changes

--- Assets/player.cs
+++ Assets/player.cs
@@ -8,20 +8,25 @@ public class player : MonoBehaviour {
     [Space(20)]
     public float upBound;
     public float lowerBound;
+    
+    Animator playerAnim;
 
     // Start is called before the first frame update
     void Start() {
-        
+        playerAnim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetAxisRaw("Vertical")>0 && transform.position.y < upBound) {
             print(transform.position.y);
+            playerAnim.SetBool("Move", true);
             transform.Translate(Vector3.up * speed * Time.deltaTime);
-        }
-        if (Input.GetAxisRaw("Vertical")<0 && transform.position.y > lowerBound) {
+        } else if (Input.GetAxisRaw("Vertical")<0 && transform.position.y > lowerBound) {
             transform.Translate(Vector3.down * speed * Time.deltaTime);
+            playerAnim.SetBool("Move", true);
+        } else {
+            playerAnim.SetBool("Move", false);
         }
     }
 }
