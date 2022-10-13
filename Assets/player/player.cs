59e9c0a77def6f8bbdf9ddc77a0fdccd9261bf50
Thomas Plans <thomas@khandaan.org>
2022-10-13 10:51:07 +0100

    death + stamina

--- Assets/player/player.cs
+++ Assets/player/player.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class player : MonoBehaviour {
+
+    [Header("Stats")]
+    public float health;
     public float speed;
+    public float power;
+    public float rechargeRate;
+    public Slider energyBar;
     [Space(20)]
     [Header("Set Boundaries")]
     public float upBound;
@@ -17,8 +24,10 @@ public class player : MonoBehaviour {
     [Header("Reaction Events")]
     public UnityEvent hit;
     public UnityEvent shoot; 
-    
+
     bool canMove = true;
+    bool canCharge = true;
+    
 
     // Start is called before the first frame update
     void Start() {
@@ -28,10 +37,12 @@ public class player : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        float powerChange = 0.0f;
         float direction = Input.GetAxisRaw("Vertical");
         if (direction>0 && transform.position.y < upBound &&canMove) {
             physAnim.SetFloat("Direction", direction);
             playerAnim.SetBool("Move", true);
+
             transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
         } else if (direction<0 && transform.position.y > lowerBound&&canMove) {
             physAnim.SetFloat("Direction", direction);
@@ -40,16 +51,34 @@ public class player : MonoBehaviour {
         } else {
             playerAnim.SetBool("Move", false);
             physAnim.SetFloat("Direction", 0);
+
+            //Idle recharge
+            if (canCharge) {
+                powerChange += rechargeRate/60;
+            }
         }
 
         if (Input.GetButtonDown("Fire1") && canMove) {
             shoot.Invoke();
+            powerChange -= 30;
+        }
+        power = Mathf.Clamp(power + powerChange, 0.0f, 100.0f);
+        energyBar.value=power;
+
+        if (power == 0) {
+            StartCoroutine("overheat");
         }
     }
 
-    public IEnumerator gotHit() {
+    IEnumerator overheat() {
+        energyBar.gameObject.transform.GetChild(0).GetComponent<Image>().color = new Color(255.0f,0.0f,0.0f,255.0f);
         canMove = false;
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(2.0f);
         canMove = true;
+        energyBar.gameObject.transform.GetChild(0).GetComponent<Image>().color = new Color(255.0f,255.0f,255.0f,255.0f);
+    }
+
+    public void gotHit() {
+        canMove = false;
     }
 }
